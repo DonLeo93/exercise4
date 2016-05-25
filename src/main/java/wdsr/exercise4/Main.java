72fa2305d897cf2949cb31eeb5ce6ecd22d97d50
DonLeo93 <raf.szczepaniak@gmail.com>
2016-05-25 10:34:45 +0200

    Done

---
+++ src/main/java/wdsr/exercise4/Main.java
@@ -0,0 +1,10 @@
+package wdsr.exercise4;
+
+import wdsr.exercise4.subscriber.JmsSubscriber;
+
+public class Main {
+	public static void main(String[] args) {
+		JmsSubscriber jmsSubscriber = new JmsSubscriber("DonLeo93.TOPIC");
+		jmsSubscriber.getMessage();
+	}
+}
---
+++ src/main/java/wdsr/exercise4/subscriber/JmsSubscriber.java
@@ -0,0 +1,57 @@
+package wdsr.exercise4.subscriber;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.jms.TopicSubscriber;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JmsSubscriber {
+	private static final Logger log = LoggerFactory.getLogger(JmsSubscriber.class);
+	
+	private Connection connection = null;
+	private Session session = null;
+	private TopicSubscriber subscriber = null;
+	private int counter = 0;
+	
+	public JmsSubscriber(final String topicName) {
+		try {
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+			connection = connectionFactory.createConnection();
+			connection.setClientID("1");
+			
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Topic topic = session.createTopic(topicName);
+			subscriber = session.createDurableSubscriber(topic, "subscriber");
+		} catch (Exception e) {
+			log.error("Error : ", e);
+		}
+	}
+	
+	public void getMessage() {
+		try {
+			subscriber.setMessageListener( message -> {
+				try {
+					connection.start();
+				} catch (Exception e) {
+					log.error("Error during connection: ",e);
+				}
+				if(message instanceof TextMessage){
+					try {
+						counter++;
+						log.info("# {} MSG: {}", counter, ((TextMessage) message).getText());
+					} catch (Exception e) {
+						log.error("Error: ", e);
+					}
+				}				
+			});
+		} catch (JMSException e) {
+			log.error("Error: ", e);
+		}
+	}
+}
